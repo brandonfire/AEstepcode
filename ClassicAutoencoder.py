ed2ac084d1cf48b24147a644bdec33dc66154699
brandonfire <chengbinhu@gmail.com>
2021-11-26 05:13:38 -0500

    Update ClassicAutoencoder.py

--- ClassicAutoencoder.py
+++ ClassicAutoencoder.py
@@ -286,12 +286,46 @@ def testaccuracywithinception():
     plt.plot(hist["accuracy"])
     plt.plot(hist["val_accuracy"])
     plt.show()
+def generateENDE(Image, AEmodel, threshold):
+    EN, IN = [],[]
+    encoded_img = AEmodel.encoder(np.asarray([Image])).numpy()
+    decoded_img = AEmodel.decoder(encoded_img).numpy()
+    baselose = sum(sum(tf.keras.losses.MSE(Image,decoded_img))).numpy()
+
+    for x in range(len(Image)):
+        for y in range(len(Image[0])):
+            for z in range(Image[0][0])):
+                Image[x][y][z] += 1
+                encoded_img = AEmodel.encoder(np.asarray([Image])).numpy()
+                decoded_img = AEmodel.decoder(encoded_img).numpy()
+                tmploss = sum(sum(tf.keras.losses.MSE(Image,decoded_img))).numpy()
+                if tmploss - baselose > threshold:
+                    EN.append([x,y,z])
+    for x in range(len(Image)):
+        for y in range(len(Image[0])):
+            for z in range(Image[0][0])):
+                Image[x][y][z] -= 1
+                encoded_img = AEmodel.encoder(np.asarray([Image])).numpy()
+                decoded_img = AEmodel.decoder(encoded_img).numpy()
+                tmploss = sum(sum(tf.keras.losses.MSE(Image,decoded_img))).numpy()
+                if tmploss - baselose > threshold:
+                    DE.append([x,y,z])
+    return EN,DE
+
+def selectfrompool(EN,DE,rate):
+    chooseEN = []
+    chooseDE = []
+    for point in EN:
+        if randomselect(rate):
+            chooseEN.append(point)
+    for point in DE:
+        if randomselect(rate):
+            chooseDE.append(point)
+    return chooseEN, chooseDE
 
 
 
-
-
-def testwithnoiseattack(Autoencoder_model,Class_model,data_dir):
+def testwithnoiseattack(Autoencoder_model,Class_model,data_dir,steps,randomselectedsamples ,noise_rate):
     autoencoder = tf.keras.models.load_model(Autoencoder_model) #load autoencoder
     classfymodel = tf.keras.models.load_model(Class_model) #load downstream classification model
     pixels = 304
@@ -331,7 +365,6 @@ def testwithnoiseattack(Autoencoder_model,Class_model,data_dir):
     x_test = False
     y_test = False
     
-    photonumber = 5
     count = 0
     
     for imgs in train_generator:
@@ -340,9 +373,9 @@ def testwithnoiseattack(Autoencoder_model,Class_model,data_dir):
         if count == 1:
             x_train = imgs[0]
             y_train = imgs[1]
-        elif count < 2:
-            x_train = np.concatenate((x_train, imgs[0]), axis=0)
-            y_train = np.concatenate((y_train, imgs[1]), axis=0)
+        #elif count < 2:
+        #    x_train = np.concatenate((x_train, imgs[0]), axis=0)
+        #    y_train = np.concatenate((y_train, imgs[1]), axis=0)
         # elif count == 4:
             # x_test = train_generator.next()[0]
         # elif count <= 5:
@@ -357,9 +390,9 @@ def testwithnoiseattack(Autoencoder_model,Class_model,data_dir):
         if count == 1:
             x_test = imgs[0]
             y_test = imgs[1]
-        elif count < 2:
-            y_test = np.concatenate((y_test, imgs[1]), axis=0)
-            x_test = np.concatenate((x_test, imgs[0]), axis=0)
+        #elif count < 2:
+        #    y_test = np.concatenate((y_test, imgs[1]), axis=0)
+        #    x_test = np.concatenate((x_test, imgs[0]), axis=0)
         # elif count == 4:
             # x_test = train_generator.next()[0]
         # elif count <= 5:
@@ -367,97 +400,119 @@ def testwithnoiseattack(Autoencoder_model,Class_model,data_dir):
         else:
             break
     print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
-    
-    noise = np.random.rand(x_train.shape[0],304,304,3) 
-    #import noise_rate to modify for steps
-    noise_rate = 0.08
-    #noise = (noise - 0.5) * noise_rate
-    noisetrain = x_train + noise*noise_rate
-    shownoise = np.random.rand(x_train.shape[0],304,304)
-    for i in range(4):
-        for x in range(len(x_train[photonumber+i])):
-            for y in range(len(x_train[photonumber+i][0])):
-                for z in range(len(x_train[photonumber+i][0][0])):
-                    if randomselect(noise_rate):
-                        noisetrain[photonumber+i][x][y][z] = noise[photonumber+i][x][y][z] + x_train[photonumber+i][x][y][z]
-                    else:
-                        noisetrain[photonumber+i][x][y][z] = x_train[photonumber+i][x][y][z]
-    encoded_imgs = autoencoder.encoder(np.asarray([x_train[photonumber]])).numpy()
-    decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
-    encoded_imgs1 = autoencoder.encoder(np.asarray([noisetrain[photonumber]])).numpy()
-    decoded_imgs1 = autoencoder.decoder(encoded_imgs1).numpy()
-    
-    classpredict = classfymodel(encoded_imgs)
-    
-    startloss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs1[0]))).numpy()
-    #loss = [startloss,startloss]
-    
-    normalloss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs[0]))).numpy()
-    
-    normalcrossloss = tf.keras.losses.binary_crossentropy(classpredict,y_train[photonumber]).numpy()
-    #showsample(x_train[4])
-    
-    #showsample(encoded_imgs[0].reshape(16,16))
-    #showsample(decoded_imgs[0])
-    
-    print(normalloss,normalcrossloss,classpredict)
-    
-    minloss = startloss
-    minimage = noisetrain[photonumber]
-    maxloss = startloss
-    maximage = noisetrain[photonumber]
-    
-    minencoded = maxencoded = encoded_imgs1[0]
-    mindecoded = maxdecoded = decoded_imgs1[0]
-    count = 0
-    steps = 2
-    for step in range(steps):
-        baseimage = maximage
-        for i in range(2):
-            noise = np.random.rand(304,304,3) 
-            tmptrain = np.zeros((304,304,3))
+    for imgs in train_generator:
+        x_train = imgs[0] #depends on the data size, this loop takes hours to days to be done.
+        y_train = imgs[1]
+        for photonumber in range(len(imgs[0])):
+            noise = np.random.rand(x_train.shape[0],304,304,3) 
+            #import noise_rate to modify for steps
+            #noise = (noise - 0.5) * noise_rate
+            noisetrain = x_train + noise*noise_rate
+            shownoise = np.random.rand(x_train.shape[0],304,304)
+            
             for x in range(len(x_train[photonumber])):
                 for y in range(len(x_train[photonumber][0])):
                     for z in range(len(x_train[photonumber][0][0])):
-                        if randomselect(noise_rate/steps):
-                            tmptrain[x][y][z] = noise[x][y][z] + baseimage[x][y][z]
-                        else:
-                            tmptrain[x][y][z] = baseimage[x][y][z]
-            count += 1
-            print(count, "images generated")
-            encoded_imgs1 = autoencoder.encoder(np.asarray([tmptrain])).numpy()
+
+
+                            if randomselect(noise_rate):
+                                noisetrain[photonumber+i][x][y][z] = noise[photonumber+i][x][y][z] + x_train[photonumber+i][x][y][z]
+                            else:
+                                noisetrain[photonumber+i][x][y][z] = x_train[photonumber+i][x][y][z]
+            encoded_imgs = autoencoder.encoder(np.asarray([x_train[photonumber]])).numpy()
+            decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+            encoded_imgs1 = autoencoder.encoder(np.asarray([noisetrain[photonumber]])).numpy()
             decoded_imgs1 = autoencoder.decoder(encoded_imgs1).numpy()
-            tmploss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs1[0]))).numpy()
-            print("Current loss:", tmploss)
-            if tmploss < minloss:
-                minloss = tmploss
-                minimage = tmptrain
-                minencoded = encoded_imgs1[0]
-                mindecoded = decoded_imgs1[0]
-            elif tmploss > maxloss:
-                maxloss = tmploss
-                maximage = tmptrain
-                maxencoded = encoded_imgs1[0]
-                maxclasssample = encoded_imgs1
-                maxdecoded = decoded_imgs1[0]
-        
-    
-    
-    
-    train = [x_train[photonumber],x_train[photonumber]]
-    encoded = [encoded_imgs[0],encoded_imgs[0]]
-    decoded = [decoded_imgs[0],decoded_imgs[0]]
-    
-    ntrain = [minimage,maximage]
-    encoded_imgs1 = [minencoded,maxencoded]
-    decoded_imgs1 = [mindecoded,maxdecoded]
-    
-    #print("maxencoded",maxencoded,maxencoded.shape)
-    maxclass = classfymodel(maxclasssample)
-    print("This is maxclass", maxclass, type(maxclasssample))
-    maxcrossloss = tf.keras.losses.binary_crossentropy(maxclass,y_train[photonumber]).numpy()
-    
-    print(minloss,maxloss,maxcrossloss,maxclass)
+            
+            classpredict = classfymodel(encoded_imgs)
+            
+            startloss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs1[0]))).numpy()
+            #loss = [startloss,startloss]
+            
+            normalloss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs[0]))).numpy()
+            
+            normalcrossloss = tf.keras.losses.binary_crossentropy(classpredict,y_train[photonumber]).numpy()
+            #showsample(x_train[4])
+            
+            #showsample(encoded_imgs[0].reshape(16,16))
+            #showsample(decoded_imgs[0])
+            
+            print(normalloss,normalcrossloss,classpredict)
+            
+            minloss = startloss
+            minimage = noisetrain[photonumber]
+            maxloss = startloss
+            maximage = noisetrain[photonumber]
+            
+            minclassprobability = classpredict[targetclass]
+            maxclassprobability = classpredict[targetclass]
+
+            minencoded = maxencoded = encoded_imgs1[0]
+            mindecoded = maxdecoded = decoded_imgs1[0]
+            count = 0
+            EN,DE = generateENDE(x_train[photonumber], autoencoder, 0.1)
+
+            
+            for step in range(steps):
+                baseimage = maximage
+                for i in range(randomselectedsamples):
+                    noise = np.random.rand(304,304,3) 
+                    tmptrain = list(x_train[photonumber])#np.zeros((304,304,3))
+                    chooseEN, chooseDE = selectfrompool(EN,DE, noise_rate/steps)
+                    for e in chooseEN:
+                        tmptrain[e[0]][e[1]][e[2]] += 1
+                    for d in chooseDE:
+                        tmptrain[d[0]][d[1]][d[2]] -= 1
+
+                    #for x in range(len(x_train[photonumber])):
+                    #    for y in range(len(x_train[photonumber][0])):
+                    #        for z in range(len(x_train[photonumber][0][0])):
+                    #            for e in chooseEN:
+
+
+                                #if randomselect(noise_rate/steps):
+                                #    tmptrain[x][y][z] = noise[x][y][z] + baseimage[x][y][z]
+                                #else:
+                                #    tmptrain[x][y][z] = baseimage[x][y][z]
+                    count += 1
+                    print(count, "images generated")
+                    encoded_imgs1 = autoencoder.encoder(np.asarray([tmptrain])).numpy()
+                    decoded_imgs1 = autoencoder.decoder(encoded_imgs1).numpy()
+                    tmploss = sum(sum(tf.keras.losses.MSE(x_train[photonumber],decoded_imgs1[0]))).numpy()
+                    classpredict = classfymodel(encoded_imgs)
+                    tmpclassprobability = classfymodel(encoded_imgs)[targetclass]
+                    print("Current loss:", tmploss)
+                    print("Current classification probability", classpredict)
+                    
+                    #if tmploss < minloss:
+                    #    minloss = tmploss
+                    #   minimage = tmptrain
+                    #    minencoded = encoded_imgs1[0]
+                    #    mindecoded = decoded_imgs1[0]
+                    if tmpclassprobability > maxclassprobability:
+                        maxclassprobability = tmpclassprobability
+                        maximage = tmptrain
+                        maxencoded = encoded_imgs1[0]
+                        maxclasssample = encoded_imgs1
+                        maxdecoded = decoded_imgs1[0]
+                
+            
+            
+            
+            train = [x_train[photonumber],x_train[photonumber]]
+            encoded = [encoded_imgs[0],encoded_imgs[0]]
+            decoded = [decoded_imgs[0],decoded_imgs[0]]
+            
+            ntrain = [minimage,maximage]
+            encoded_imgs1 = [minencoded,maxencoded]
+            decoded_imgs1 = [mindecoded,maxdecoded]
+            
+            #print("maxencoded",maxencoded,maxencoded.shape)
+            maxclass = classfymodel(maxclasssample)
+            print("This is maxclass", maxclass, type(maxclasssample))
+            maxcrossloss = tf.keras.losses.binary_crossentropy(maxclass,y_train[photonumber]).numpy()
+            
+            print(minloss,maxloss,maxcrossloss,maxclass)
     
     plt.figure(figsize=(20, 4))
     n = 2
@@ -544,4 +599,8 @@ if __name__ == "__main__":
     #testaccuracywithinception()
     Autoencoder_model,Class_model = "./ClassicalEncodermodel","./FirstEnThenClassModel"
     data_dir = '.\\FlowerData\\flower_photos'
-    testwithnoiseattack(Autoencoder_model,Class_model,data_dir)#Attack test.
+    steps = 500
+    randomselectedsamples = 100
+    noise_rate = 0.08
+    targetclass = 2
+    testwithnoiseattack(Autoencoder_model,Class_model,data_dir,steps,randomselectedsamples, noise_rate , targetclass)#Attack test.
